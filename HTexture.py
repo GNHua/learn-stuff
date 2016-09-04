126cc927c7b9265af2f4fe0229a976abd8e6c8ae
GNHua <gonghuabupt@gmail.com>
2016-09-04 16:15:08 -0600

    init push, finished lesson 5

---
+++ HTexture.py
@@ -0,0 +1,47 @@
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+
+class HTexture:
+    def __init__(self):
+        self.mTextureID = 0
+        self.mTextureWidth = 0
+        self.mTextureHeight = 0
+        
+    def loadTextureFromPixels32(self, pixels, w, h):
+        self.freeTexture()
+        self.mTextureWidth = w
+        self.mTextureHeight = h
+        
+        self.mTextureID = glGenTextures(1)
+        glBindTexture(GL_TEXTURE_2D, self.mTextureID)
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE, w, h, 0, GL_LUMINANCE, GL_UNSIGNED_BYTE, pixels)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+        glBindTexture(GL_TEXTURE_2D, 0)
+        
+        error = glGetError()
+        if error != GL_NO_ERROR:
+            print("Error loading texture")
+            return False
+        return True
+        
+    def freeTexture(self):
+        if self.mTextureID != 0:
+            glDeleteTextures(1, self.mTextureID)
+            self.mTextureID = 0
+        self.mTextureWidth = 0
+        self.mTextureHeight = 0
+        
+    def render(self, x, y):
+        if self.mTextureID != 0:
+            glLoadIdentity()
+            glTranslatef(x, y, 0.)
+            glBindTexture(GL_TEXTURE_2D, self.mTextureID)
+            
+            glBegin(GL_QUADS)
+            glTexCoord2f( 0., 0. ); glVertex2f(                 0.,                  0. );
+            glTexCoord2f( 1., 0. ); glVertex2f( self.mTextureWidth,                  0. );
+            glTexCoord2f( 1., 1. ); glVertex2f( self.mTextureWidth, self.mTextureHeight );
+            glTexCoord2f( 0., 1. ); glVertex2f(                 0., self.mTextureHeight );
+            glEnd()
---
+++ HUtil.py
@@ -0,0 +1,58 @@
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+from HTexture import HTexture
+import numpy as np
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+SCREEN_FPS = 30
+gCheckerBoardTexture = HTexture()
+
+def initGL():
+    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
+    
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(0., SCREEN_WIDTH, SCREEN_HEIGHT, 0., 1., -1.)
+    
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    glClearColor(0., 0., 0., 1.)
+    glEnable(GL_TEXTURE_2D)
+    
+    error = glGetError()
+    if error != GL_NO_ERROR:
+        print('Error initializing OpenGL')
+        return False
+    return True
+    
+def loadMedia():
+    CHECKERBOARD_WIDTH = 128
+    CHECKERBOARD_HEIGHT = 128
+    # checkerBoard = np.zeros((CHECKERBOARD_HEIGHT, CHECKERBOARD_WIDTH), dtype=np.uint32)
+    # color1 = 0xFFFFFFFF
+    # color2 = 0xFF0000FF
+    # c = np.fromfunction(lambda x, y: (x//16 + y//16)%2, (CHECKERBOARD_HEIGHT, CHECKERBOARD_WIDTH), dtype=np.uint8)
+    # checkerBoard[c == 0] = color1
+    # checkerBoard[c == 1] = color2
+    # checkerBoard = np.fromfunction(lambda x, y: (x//16 + y//16)%2, (CHECKERBOARD_HEIGHT, CHECKERBOARD_WIDTH), dtype=np.uint8) * 127 + \
+    #     np.ones((CHECKERBOARD_HEIGHT, CHECKERBOARD_WIDTH), dtype=np.uint8) * 128
+    checkerBoard = np.zeros((CHECKERBOARD_HEIGHT, CHECKERBOARD_WIDTH), dtype=np.uint8)
+    checkerBoard[20,:] = np.ones(CHECKERBOARD_WIDTH, dtype=np.uint8) * 255
+    checkerBoard[:,20] = np.ones(CHECKERBOARD_HEIGHT, dtype=np.uint8) * 255
+    
+    if not gCheckerBoardTexture.loadTextureFromPixels32(np.ascontiguousarray(checkerBoard).data, CHECKERBOARD_WIDTH, CHECKERBOARD_HEIGHT):
+        print('Unable to load checkerboard texture!')
+        return False
+    return True
+    
+def update():
+    pass
+    
+def render():
+    glClear(GL_COLOR_BUFFER_BIT)
+    x = (SCREEN_WIDTH - gCheckerBoardTexture.mTextureWidth)/2
+    y = (SCREEN_HEIGHT - gCheckerBoardTexture.mTextureHeight)/2
+    gCheckerBoardTexture.render(x, y)
+    glutSwapBuffers()

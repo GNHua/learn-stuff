05bb0899441c19e553f705011fffbf79ddfb768b
GNHua <gonghuabupt@gmail.com>
2017-02-19 16:41:47 -0700

    init push

---
+++ Joey_de_Vries/01_create_a_window/main.py
@@ -0,0 +1,30 @@
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+import sys
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+def render():
+    glClear(GL_COLOR_BUFFER_BIT)
+    glutSwapBuffers()
+    
+def handleKeys(key, x, y):
+    pass
+
+def main():
+    glutInit(sys.argv)
+    glutInitContextVersion(3, 1)
+    glutInitDisplayMode( GLUT_DOUBLE )
+    glutInitWindowSize(SCREEN_WIDTH, SCREEN_HEIGHT)
+    glutCreateWindow(b'OpenGL')
+    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
+    glClearColor(0., 0., 0., 1.)
+    glutDisplayFunc(render)
+    glutKeyboardFunc(handleKeys)
+    glutMainLoop()
+    return 0
+    
+if __name__ == '__main__':
+    main()
---
+++ Joey_de_Vries/README.md
@@ -0,0 +1,7 @@
+Going through Joey de Vries' OpenGL Tutorial with PyOpenGL
+
+Instead of using `glfw`, I am trying to `glut`, because it's much more easy to install for Python
+
+* Using anaconda Python 3.5 on MacOSX
+* https://learnopengl.com/
+* [Joey's patreon link](https://www.patreon.com/learnopengl)

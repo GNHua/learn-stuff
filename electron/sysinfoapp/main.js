dcee54fe09104538b6b903ab3f5d552726f66356
GNHua <gonghuabupt@gmail.com>
2018-08-31 16:20:32 -0600

    finshed

--- electron/sysinfoapp/main.js
+++ electron/sysinfoapp/main.js
@@ -3,28 +3,27 @@ const path = require('path');
 const url = require('url');
 
 // init window
-let win;
+let win
 
 function createWindow() {
     win = new BrowserWindow({
         width: 800, 
-        height: 600, 
-        icon: path.join(__dirname, 'gear.svg')
-    });
+        height: 600
+    })
 
     // Load index.html
     win.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file',
         slashes: true
-    }));
+    }))
 
     // Open devtools
     win.webContents.openDevTools();
 
     win.on('closed', () => {
         win = null;
-    });
+    })
 }
 
 // Run createWindow function
@@ -33,6 +32,14 @@ app.on('ready', createWindow);
 // Quit when all windows are closed
 app.on('window-all-closed', () => {
     if(process.platform !== 'darwin') {
-        app.quit();
+        app.quit()
     }
-});
+})
+
+app.on('activate', function () {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+        createWindow();
+    }
+})

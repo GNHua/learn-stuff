2386a33c6445af7d98c8a06bf68c3898604910a1
GNHua <gonghuabupt@gmail.com>
2018-09-01 18:10:06 -0600

    indent 2 spaces

--- electron/sysinfoapp/main.js
+++ electron/sysinfoapp/main.js
@@ -6,24 +6,24 @@ const url = require('url');
 let win
 
 function createWindow() {
-    win = new BrowserWindow({
-        width: 800, 
-        height: 600
-    })
+  win = new BrowserWindow({
+    width: 800, 
+    height: 600
+  })
 
-    // Load index.html
-    win.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file',
-        slashes: true
-    }))
+  // Load index.html
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file',
+    slashes: true
+  }))
 
-    // Open devtools
-    win.webContents.openDevTools();
+  // Open devtools
+  win.webContents.openDevTools();
 
-    win.on('closed', () => {
-        win = null;
-    })
+  win.on('closed', () => {
+    win = null;
+  })
 }
 
 // Run createWindow function
@@ -31,15 +31,15 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed
 app.on('window-all-closed', () => {
-    if(process.platform !== 'darwin') {
-        app.quit()
-    }
+  if(process.platform !== 'darwin') {
+    app.quit()
+  }
 })
 
 app.on('activate', function () {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (win === null) {
-        createWindow();
-    }
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow();
+  }
 })
